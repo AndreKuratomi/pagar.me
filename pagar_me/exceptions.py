56bebd07de500e3d04e26cf7a2a488f8e3feaa46
André Kuratomi <andrekuratomi@gmail.com>
2022-03-28 09:56:44 -0300

    fix: login issue finally solved

--- pagar_me/exceptions.py
+++ pagar_me/exceptions.py
@@ -1,6 +1,15 @@
 from rest_framework.exceptions import APIException
 
 
+# class UserAlreadyExistsError(APIException):
+#     status_code = '422'
+#     default_detail = {"message": ["This user already exists"]}
+
+
+# class PatientAlreadyExistsError(APIException):
+#     status_code = '422'
+#     default_detail = {"message": ["This patient already exists"]}
+
 class InvalidCredentialsError(APIException):
     status_code = '401'
     default_detail = {"message": ["Invalid credentials"]}
